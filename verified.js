da6e5c0a9ba38a8448daaa8d16cd5984a0cd0dcc
Neev Sahay <neevsahay@gmail.com>
2023-10-25 10:33:54 +0530

    resourcs done

--- verified.js
+++ verified.js
@@ -1,7 +1,18 @@
 var pages={
-    verified: `<p class="verified">Verified... Kevin Baba.</p>
-    <div class="content-container">
-    <p class="calls">CallSign: ScreamingEagle</p>
-    <p class="calls">Phrase: Cosmos is calling</p>
-    </div>`
+    verified: `<p class="verified">Verified<span class="loading">...</span> Kevin Baba.</p>`,
+    headlines:`<p class="headlines-heading">Headlines</p>
+    <ol>
+     <li class="list">WANTED: Drayvok, Martian, 5’4’, Reddish brown </li>
+     <span class="list-date">03/05/3033</span>
+     <li class="list">Recruiting Weaponsmiths: Contact NERV HQ </li>
+     <span class="list-date">01/05/3033</span>
+     <li class="list">Stategical Meeting Scheduled: location 5E12XG </li>
+     <span class="list-date">28/04/3033</span>
+    </ol> `,
+    resources:`<p class="headlines-heading">Resources</p>
+    <ul>
+        <li class='list'>Defence Weapon Inventory</li>
+        <li class='list'>Inter-Planetary Allies</li>
+        <li class='list'>Defence House Locations</li>
+    </ul>`
 }
