07dcf45c472056660e4132e5b72311e9e1463730
Neev Sahay <neevsahay@gmail.com>
2023-10-25 09:56:26 +0530

    verified done

---
+++ verified.js
@@ -0,0 +1,7 @@
+var pages={
+    verified: `<p class="verified">Verified... Kevin Baba.</p>
+    <div class="content-container">
+    <p class="calls">CallSign: ScreamingEagle</p>
+    <p class="calls">Phrase: Cosmos is calling</p>
+    </div>`
+}
