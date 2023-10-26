1ba579193bfb7dd632cff8216f2b6df02084e932
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2023-10-26 16:46:08 +0530

    sreenath 3d coder

--- 3d.js
+++ 3d.js
@@ -1,79 +1,33 @@
-// import * as THREE from 'three'
-// import { LoadGLTFByPath } from './Helpers/ModelHelper.js'
+import * as THREE from 'three'
+import { LoadGLTFByPath } from './Helpers/ModelHelper.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// //Renderer does the job of rendering the graphics
-// let renderer = new THREE.WebGLRenderer({
+let renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg'),
+  antialias: true,
+});
 
-// 	//Defines the canvas component in the DOM that will be used
-// 	canvas: document.querySelector('#bg'),
-//   antialias: true,
-// });
-
-// renderer.setSize(window.innerWidth, window.innerHeight);
-
-// //set up the renderer with the default settings for threejs.org/editor - revision r153
-// renderer.shadows = true;
-// renderer.shadowType = 1;
-// renderer.shadowMap.enabled = true;
-// renderer.setPixelRatio( window.devicePixelRatio );
-// renderer.toneMapping = 0;
-// renderer.toneMappingExposure = 1
-// renderer.useLegacyLights  = true;
-// renderer.toneMapping = THREE.NoToneMapping;
-// renderer.setClearColor(0xffffff, 0);
-// //make sure three/build/three.module.js is over r152 or this feature is not available. 
-// renderer.outputColorSpace = THREE.SRGBColorSpace 
-
-// const scene = new THREE.Scene();
-
-// scene.scale.x = 1000;
-// scene.scale.y = 1000;
-// scene.scale.z = 1000;
-
-// let cameraList = [];
-
-// let camera;
-
-// // Load the GLTF model
-// LoadGLTFByPath(scene)
-//   .then(() => {
-//     retrieveListOfCameras(scene);
-//   })
-//   .catch((error) => {
-//     console.error('Error loading JSON scene:', error);
-//   });
-
-// //retrieve list of all cameras
-// function retrieveListOfCameras(scene){
-//   // Get a list of all cameras in the scene
-//   scene.traverse(function (object) {
-//   console.log(object)
-//     if (object.isCamera) {
-//       cameraList.push(object);
-//     }
-//   });
-
-//   //Set the camera to the first value in the list of cameras
-//   camera = new THREE.PerspectiveCamera(60,1,0.1,1000);
-//   camera.position.set(-0.000231, -1.47613, 1.2435);
-
-//   updateCameraAspect(camera);
-
-//   // Start the animation loop after the model and cameras are loaded
-//   animate();
-// }
-
-// // Set the camera aspect ratio to match the browser window dimensions
-// function updateCameraAspect(camera) {
-//   const width = window.innerWidth;
-//   const height = window.innerHeight;
-//   camera.aspect = width / height;
-//   camera.updateProjectionMatrix();
-// }
-
-// //A method to be run each time a frame is generated
-// function animate() {
-//   requestAnimationFrame(animate);
-
-//   renderer.render(scene, camera);
-// };
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadows = true;
+renderer.shadowType = 1;
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.toneMapping = 0;
+renderer.toneMappingExposure = 1
+renderer.useLegacyLights  = true;
+renderer.toneMapping = THREE.NoToneMapping;
+renderer.setClearColor(0xffffff, 0);
+renderer.outputColorSpace = THREE.SRGBColorSpace 
+const scene = new THREE.Scene();
+LoadGLTFByPath(scene);
+const camera = new THREE.PerspectiveCamera(60,1,0.1,1000);
+camera.position.setX(1);
+camera.position.setY(1);
+camera.position.setZ(1);
+const controls = new OrbitControls( camera, renderer.domElement);
+animate();
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+};
